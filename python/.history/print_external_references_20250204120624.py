c52c4f6108104ed25ed5ebd3b2e0f65dfe41f1c9
Clement Granjon <clement.granjon@icloud.com>
2025-02-12 15:53:36 +0000

    changes

---
+++ python/.history/print_external_references_20250204120624.py
@@ -0,0 +1,18 @@
+import hou
+
+# Execute the opextern command
+command = "opextern -gRrm /" 
+# command = "opextern -rR /" # Prints all externals references
+result = hou.hscript(command)
+
+# result is a tuple where the first element is the command's stdout
+output = result[0]
+external_files = output.strip().split('\n')
+
+
+output = hou.fileReferences()
+
+# Print or process the output
+print(output)
+
+# print(str(len(output))+" external files.")
